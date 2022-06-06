c2987e2eb9db115cdf72bcf1322b8f76bedb9cbd
canicemichael <canicemichael@gmail.com>
2022-06-06 23:29:41 +0100

    update

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname+"/public")))
 
-io.on("connection", (socket) => {
-    socket.on("newuser", (username) => {
+io.on("connection", function(socket) {
+    socket.on("newuser", function(username) {
         socket.broadcast.emit("update", username + " joined the conversation");
     });
-    socket.on("exituser", (username) => {
+    socket.on("exituser", function(username) {
         socket.broadcast.emit("update", username + " left the conversation");
     });
-    socket.on("chat", (message) => {
+    socket.on("chat", function(message) {
         socket.broadcast.emit("chat", message); 
     })
 })
